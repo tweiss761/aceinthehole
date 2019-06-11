518b35fe8291924347a10898bb9bfc25446dcb82
tweiss761 <49891487+tweiss761@users.noreply.github.com>
2019-06-10 22:00:20 -0700

    Add files via upload

---
+++ template/faqs.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<!--
+Assignment Name: ace in the hole template
+File Name: template.php
+Date: 4/22/18
+Programmer: Tammy Weiss
+-->
+<html lang="en">
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="utf-8">
+    <meta name="author" content="Tammy Weiss">
+    <title>Ace in the Hole MultiSport Event</title>
+
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet" type="text/css">
+    <link href="css/reset.css" rel="stylesheet" type="text/css">
+    <link href="css/helper.css" rel="stylesheet" type="text/css">
+    <link href="css/grid.css" rel="stylesheet" type="text/css">
+    <link href="css/style.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
+</head>
+
+<body>
+    <?php include 'includes/nav.inc.html.php';?>
+    <?php include 'includes/header.inc.html.php';?>
+
+        
+   
+    <div class="section group">
+        <div class="col span_6_of_12">
+        <img src="images/mountain-bikes.jpg" alt="Bikers" class="image" title="Bikers.">
+        </div>
+        <div class="col span_6_of_12">
+        <img src="images/marathon3.jpg" alt="More runners" class="image" title="More runners.">
+        </div>
+    </div>
+    <main>
+        <h2>Course Details</h2>
+        <p>WATER TEMPERATURE is expected to be between 62 and 66 degrees. The temperature will be taken on Friday and the morning of the race. Wetsuits are recommended.<br> <br>
+         <h2>Saturday Events</h2>
+         <h2>Long Course</h2>
+        <p>Long Course SWIM- 1.2mi - Participants will make two counter-clockwise loops. Large buoys will mark the turn points. Kayakers will be positioned on the water to support the swimmers. Medical support will be present on the beach.</p>
+        <p>Long Course BIKE- 58 Miles: A scenic point-to-point course that travels over gently rolling hills prior to three hard climbs. The bike course will be marked with large directional signage and there will be course marshals at key intersections to help direct you.</p>
+        <p>Long Course RUN- 13.1mi - A mostly flat loop course on widely paved bike paths that traverse through and around this beautiful and scenic destination resort (two hills total with a minimal elevation gain).</p>
+         <h2>Olympic</h2>
+        <p>OLYMPIC SWIM- 1,500 meters - Participants will make two counter-clockwise loops. Large buoys will mark the turn points. Kayakers will be positioned on the water to support the swimmers. Medical support will be present on the beach.</p>
+        <p>OLYMPIC BIKE- 28mi - A scenic point-to-point course that travels over gently rolling hills. The bike course will be marked with large directional signage and course marshals will be present at key intersections.</p>
+        <p>OLYMPIC RUN- 10K - A mostly flat loop course on widely paved bike paths that traverse through and around this beautiful and scenic destination resort (one hill total with a minimal elevation gain).</p><br>
+          <h2>Half Marathon Course</h2>
+        <p>Half Marathon COURSE: Half-Marathon (13.1-miles): Half-Marathon event starts and finishes in the Athletes Village to the cheers of the enthusiastic crowd. Once finished, runners can enjoy the finish line festivities, including the Sports & Fitness Expo and live entertainment. Post-race refreshments will be provided and the Awards Ceremony for the Half-Marathon will begin once the results have been certified.</p><br>
+          <h2>10K Course</h2>
+        <p>10k COURSE: The 10K event starts and finishes in the Athletes Village. The paths are approximately 6 ft wide, perfectly paved and wind around through the forest. Each course has only two small hills with a minimal elevation gain to navigate and a fast downhill to flat finish to the roaring cheers of the crowd.</p><br>
+        <h2>Sunday Events</h2>
+         <h2>Sprint</h2>
+        <p>Sprint: Course will offer a 1-loop 1/2 mile swim, the exact same 28 mile Bike Course as the Olympic distance and a 5km run.</p><br>
+        <h2>Splash n' Dash</h2>
+        <p>Splash n' Dash: Kids event for participants ages 4 to 15. This is a 100 yard swim and a 50 yard dash.<br></p>
+        <h2>Try-A-Tri</h2>
+        <p>Try-A-Tri: This novice race is designed for the first time triathlete, those new to the sport, our Junior Triathletes. The swim is a shorter, more manageable 1/4 mile distance, (instead of the standard 1/2-mile Sprint distance swim), 10 mile bike ride (vs 12 miles and it's a 2 loop course making it very spectator friendly!) and a flat 2 mile run (vs 3 mile sprint course).</p><br>
+
+        <h2>FAQs</h2>
+        <h2>What are the rules?</h2>
+          <p>We currently adhere to USAT Rules for Triathlon and Duathlon.  Important rules include no drafting, you must wear a helmet and music is ABSOLUTELY NOT allowed during the bike for obvious safety reasons.<br> <br>
+        <h2>Can I use a personal music device while cycling?</h2>
+        <p>Absolutely no music devices are allowed during the bike segment for obvious safety risks and will result in immediate disqualification.</p>
+        <h2>Can I use a personal music device while running?</h2>
+          <p>During any running segments, we prefer for athletes to NOT use music devices to maximize safety, assure a fair competitive environment and assure athletes can hear all course marshal instructions. With that said, we also understand that many athletes rely on music to help endure the challenge of running. To accommodate the needs of our athletes while still assuring maximum safety and a fair, competitive environment, here is the arrangement that we can accommodate.<br> <br>
+             If you chose to use a personal music device, you must always keep one ear open for instructions so only one ear-bud can be used at any time. In addition, if you opt to use music, you will not be eligible for awards, points and rankings. So as an athlete, you have to make a choice music or awards/points/rankings.<br> <br>
+             If you are using a personal music devise with two earbuds, you will be immediately disqualified. Trust us, we don't want to have to enforce a disqualification but in order to assure the safety of all athletes, this is a very important rule.
+             We also request that if you chose to use a personal music device that you keep the volume to a minimum to assure all instructions can be heard.</p><br>
+        <h2>Do I need to wear a wetsuit?</h2>
+        <p>No, you do not need to wear a wetsuit. Many will not wear a wetsuit while others will choose to wear a wetsuit because of the buoyancy and warmth factor.</p><br>
+        <h2>Do I have to use a road or racing bike?</h2>
+           <p>No. We welcome any type of bike as long as it is functioning properly with brakes and endcaps at the end of your handlebars.</p><br>
+        <br>
+        <h2>Packet Pick Up</h2>
+        <p>All packet pick up will occur at Why Worry Racing, 123 NW Everett, Portland OR. Packet pick up hours: Thursday 8-5, Friday 9-noon. No day of event packet pick up.
+        </p>
+        <div class="section group">
+            <div class="col span_6_of_12">
+            <img src="images/marathon.jpg" alt="Marathoners" class="image" title="Marathoners.">
+            </div>
+            <div class="col span_6_of_12">
+            <img src="images/racingmore.jpg" alt="Racing of all ages" class="image" title="Racing of all ages.">
+            </div>
+        </div>
+
+    </main>
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="scripts/menu.js"></script>
+	<?php include 'includes/footer.inc.html.php'; ?>
+
+    
+
+</body>
+
+</html>
---
+++ template/index.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<!--
+Assignment Name: ace in the hole template
+File Name: template.php
+Date: 4/22/18
+Programmer: Tammy Weiss
+-->
+<html lang="en">
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="utf-8">
+    <meta name="author" content="Tammy Weiss">
+    <title>Ace in the Hole MultiSport Event</title>
+
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet" type="text/css">
+    <link href="css/reset.css" rel="stylesheet" type="text/css">
+    <link href="css/helper.css" rel="stylesheet" type="text/css">
+    <link href="css/grid.css" rel="stylesheet" type="text/css">
+    <link href="css/style.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+<div id="fb-root"></div>
+<script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.3"></script>
+
+    <?php include 'includes/nav.inc.html.php';?>
+    <?php include 'includes/header.inc.html.php';?>
+
+        
+   
+    <div class="section group">
+        <div class="col span_6_of_12">
+        <img src="images/childrenrunning.jpg" alt="Children running" class="image" title="Children running.">
+        </div>
+        <div class="col span_6_of_12">
+        <img src="images/swimming5.jpg" alt="Many swimmers" class="image" title="Many swimmers.">
+    </div>
+    </div>
+    <main>
+        <h2>About Ace In The Hole Multisport Events </h2>
+        <p>Ace in the Hole Multisport Events is proud to offer running and triathlon events to athletes of all shapes and sizes, national origins, sexual orientations and cultural
+            backgrounds.  We offer Events for Every Body.</p>
+        <h2>About The Event</h2>
+        <p>The Annual Ace in the Hole Multisport Weekend is a legendary event in the Oregon triathlon and running community.  It has become a traditional destination race for athletes from across the nation.  There is something for every level of athletic ability.  The weekend includes a first timer triathlon,a sprint, Olympic, and Half-Iron triathlons and 10K and Half marathon runs.  Come to experience your first race orcome to compete to win, but make sure you come to have fun! </p>
+
+        <h2>What to Bring</h2>
+        <p>Watch the weather closely. The show goes on no matter what the weather is doing.  You must wear your chip timing piece during the entire event.  You will be given a band that will hold your chip timing piece around your ankle throughout the entire event.  Be sure that it is snapped tightly.  Be sure to have your chip timing piece on before you start the race and be sure to step over the timing mats after each segment of the race. If you are wearing a wetsuit, make sure the timing piece goes under your wetsuite otherwise, you will have a very difficult time getting off your wetsuit.</p>
+
+        <h2>Swimming</h2>
+          <p> Wetsuits are optional for the swim but will provide buoyancy and warmth.  However, many people opt for no wetsuit for a triathlon so no worries.  We will provide you with a swim cap but you will want to bring your own goggles.<br> <br>
+        <h2>Bike</h2>
+          <p>A biking helmet is mandatory.  You will also receive 2 stickers in your package with your race number on them.  The small sticker should go on the front of your helmet.  The bigger sticker will wrap around your bike frame.  Road or mountain bikes are acceptable.  For safety reasons, be sure to have end-caps on the end of your handle-bars.</p><br> <br>
+        <h2>Run</h2>
+          <p>You must finish the race with your bib number on the front of you.  Some people choose to pin it on at the beginning of the race and have it on for the whole event so they don't have to worry about it.  Others pin it on a singlet that they put on once they finish the swim before they head out for the bike.  Others use an elastic racing strap that they pin their bib number to and then quickly strap it on before they leave the run.  Use whatever option feels best for you.</p>
+        <div class="section group">
+            <div class="col span_6_of_12">
+            <img src="images/allages1.jpg" alt="Runners of all ages" class="image" title="Runners of all Ages.">
+            </div>
+            <div class="col span_6_of_12">
+            <img src="images/triathlon.jpg" alt="Triathlon fun" class="image" title="Triathlon fun.">
+            </div>
+        </div>
+        <h2>Staying Connected!</h2>
+        <div class="section group">
+            <div class="col span_4_of_12">
+                <div class="fb-page" data-href="https://www.facebook.com/cas222cascade/" data-tabs="timeline" data-width="340" data-height="500" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/cas222cascade/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/cas222cascade/">CAS 222</a></blockquote></div>
+            </div>
+            <div class="col span_4_of_12">
+                <a class="twitter-timeline" data-width="340" data-height="500" href="https://twitter.com/pcccas222?ref_src=twsrc%5Etfw">Tweets by pcccas222</a>
+                <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+            </div>
+            <div class="col span_4_of_12">
+                <a class="weatherwidget-io" href="https://forecast7.com/en/45d52n122d68/portland/?unit=us" data-label_1="PORTLAND" data-label_2="WEATHER" data-theme="original" >PORTLAND WEATHER</a>
+                <script>
+                    !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src='https://weatherwidget.io/js/widget.min.js';fjs.parentNode.insertBefore(js,fjs);}}(document,'script','weatherwidget-io-js');
+                </script>
+            </div>
+            </div>
+
+    </main>
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="scripts/menu.js"></script>
+	<?php include 'includes/footer.inc.html.php';?>
+
+
+</body>
+
+</html>
